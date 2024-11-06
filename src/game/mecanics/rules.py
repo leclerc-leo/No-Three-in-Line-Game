09b11538d162ca7d7f6ebd4e6083b1b867cb26fd
Léo LECLERC <leo@leclerc.dev>
2024-11-06 01:28:09 +0100

    fix: Improve get_three_in_line return type to be a list of set instead of a list of tuples, to make it easier to compare
    - Added tests for get_three_in_line

--- src/game/mecanics/rules.py
+++ src/game/mecanics/rules.py
@@ -20,12 +20,12 @@ def _is_on_same_line(
 def get_three_in_line(
     played: set[tuple[int, int]],
     board: set[tuple[int, int]] | None = None,
-) -> list[tuple[tuple[int, int], ...]]:
+) -> list[set[tuple[int, int]]]:
 
     # Maybe possible to use the board to optimize the function
     board = board or set()
 
-    results: list[tuple[tuple[int, int], ...]] = []
+    results: list[set[tuple[int, int]]] = []
 
     # Use set to ensure unique combinations of 3 points
     for comb in set(itertools.combinations(played, 3)):
@@ -33,6 +33,6 @@ def get_three_in_line(
         # Check if these three points are on the same line
         if _is_on_same_line(*comb):
 
-            results.append(comb)  # noqa: PERF401
+            results.append(set(comb))  # noqa: PERF401
 
     return results
--- tests/unit/mecanis/test_rules.py
+++ tests/unit/mecanis/test_rules.py
@@ -54,6 +54,7 @@ def test_diagonal_lines() -> None:
         (10, 10),
     )
 
+
 def test_discrete_diagonal_lines() -> None:
 
     assert rules._is_on_same_line(
@@ -67,3 +68,41 @@ def test_discrete_diagonal_lines() -> None:
         (6, 3),
         (20, 10),
     )
+
+
+def test_get_three_in_line_no_lines() -> None:
+
+    played = {(0, 0), (1, 1), (2, 0)}
+
+    assert rules.get_three_in_line(played) == []
+
+
+def test_get_three_in_line_horizontal() -> None:
+
+    played = {(0, 0), (1, 0), (2, 0)}
+
+    assert rules.get_three_in_line(played) == [{(0, 0), (1, 0), (2, 0)}]
+
+
+def test_get_three_in_line_vertical() -> None:
+
+    played = {(0, 0), (0, 1), (0, 2)}
+
+    assert rules.get_three_in_line(played) == [{(0, 0), (0, 1), (0, 2)}]
+
+
+def test_get_three_in_line_diagonal() -> None:
+
+    played = {(0, 0), (1, 1), (2, 2)}
+
+    assert rules.get_three_in_line(played) == [{(0, 0), (1, 1), (2, 2)}]
+
+
+def test_get_three_in_line_multiple_lines() -> None:
+
+    played = {(0, 0), (1, 1), (2, 2), (0, 1), (0, 2)}
+    result = rules.get_three_in_line(played)
+
+    expected = [{(0, 0), (1, 1), (2, 2)}, {(0, 0), (0, 1), (0, 2)}]
+
+    assert sorted(result) == sorted(expected)
